ee25c00da0eb884d383db78af064cfaba6438bf9
Eduardo Souza <edcampossouza@gmail.com>
2022-11-16 19:34:57 -0300

    updated game logic

--- src/logic/game.js
+++ src/logic/game.js
@@ -13,21 +13,35 @@ function printTable(table) {
 function generateGame(height, width, bombs) {
   const inBounds = (x, y) => x >= 0 && y >= 0 && x < height && y < width;
   const oneIfBomb = (x, y) => (inBounds(x, y) && table[x][y] === "b" ? 1 : 0);
-  const zeroCell = (x, y) => inBounds(x, y) && table[x][y] === 0;
+  const safeCell = (x, y) => inBounds(x, y) && typeof table[x][y] === "number";
   const lost = () => {
     for (let i = 0; i < height; i++) {
       for (let j = 0; j < width; j++) {
         state[i][j] = "r";
       }
     }
+    stats.result = "lost";
+  };
+  const stats = {
+    bombsRevealed: 0,
+    result: null,
+    plays: 0,
   };
   let plays = 0;
   let bombsRevealed = 0;
   let result = "";
+  const insertCellInArray = (cell, arr) => {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i].x === cell.x && arr[i].y === cell.y) {
+        return;
+      }
+    }
+    arr.push(cell);
+  };
   const tryCell = (x, y) => {
     if (inBounds(x, y)) {
       if (state[x][y] === "r") return;
-      plays++;
+      stats.plays++;
       if (table[x][y] === "b") {
         lost();
       } else {
@@ -37,52 +51,76 @@ function generateGame(height, width, bombs) {
           state[toReveal.x][toReveal.y] = "r";
           arrToReveal.splice(0, 1);
           if (
-            zeroCell(toReveal.x - 1, toReveal.y - 1) &&
+            safeCell(toReveal.x - 1, toReveal.y - 1) &&
             state[toReveal.x - 1][toReveal.y - 1] !== "r"
           ) {
-            arrToReveal.push({ x: toReveal.x - 1, y: toReveal.y - 1 });
+            insertCellInArray(
+              { x: toReveal.x - 1, y: toReveal.y - 1 },
+              arrToReveal
+            );
           }
           if (
-            zeroCell(toReveal.x - 1, toReveal.y) &&
+            safeCell(toReveal.x - 1, toReveal.y) &&
             state[toReveal.x - 1][toReveal.y] !== "r"
           ) {
-            arrToReveal.push({ x: toReveal.x - 1, y: toReveal.y });
+            insertCellInArray(
+              { x: toReveal.x - 1, y: toReveal.y },
+              arrToReveal
+            );
           }
           if (
-            zeroCell(toReveal.x - 1, toReveal.y + 1) &&
+            safeCell(toReveal.x - 1, toReveal.y + 1) &&
             state[toReveal.x - 1][toReveal.y + 1] !== "r"
           ) {
-            arrToReveal.push({ x: toReveal.x - 1, y: toReveal.y + 1 });
+            insertCellInArray(
+              { x: toReveal.x - 1, y: toReveal.y + 1 },
+              arrToReveal
+            );
           }
           if (
-            zeroCell(toReveal.x, toReveal.y - 1) &&
+            safeCell(toReveal.x, toReveal.y - 1) &&
             state[toReveal.x][toReveal.y - 1] !== "r"
           ) {
-            arrToReveal.push({ x: toReveal.x, y: toReveal.y - 1 });
+            insertCellInArray(
+              { x: toReveal.x, y: toReveal.y - 1 },
+              arrToReveal
+            );
           }
           if (
-            zeroCell(toReveal.x, toReveal.y + 1) &&
+            safeCell(toReveal.x, toReveal.y + 1) &&
             state[toReveal.x][toReveal.y + 1] !== "r"
           ) {
-            arrToReveal.push({ x: toReveal.x, y: toReveal.y + 1 });
+            insertCellInArray(
+              { x: toReveal.x, y: toReveal.y + 1 },
+              arrToReveal
+            );
           }
           if (
-            zeroCell(toReveal.x + 1, toReveal.y - 1) &&
+            safeCell(toReveal.x + 1, toReveal.y - 1) &&
             state[toReveal.x + 1][toReveal.y - 1] !== "r"
           ) {
-            arrToReveal.push({ x: toReveal.x + 1, y: toReveal.y - 1 });
+            insertCellInArray(
+              { x: toReveal.x + 1, y: toReveal.y - 1 },
+              arrToReveal
+            );
           }
           if (
-            zeroCell(toReveal.x + 1, toReveal.y) &&
+            safeCell(toReveal.x + 1, toReveal.y) &&
             state[toReveal.x + 1][toReveal.y] !== "r"
           ) {
-            arrToReveal.push({ x: toReveal.x + 1, y: toReveal.y });
+            insertCellInArray(
+              { x: toReveal.x + 1, y: toReveal.y },
+              arrToReveal
+            );
           }
           if (
-            zeroCell(toReveal.x + 1, toReveal.y + 1) &&
+            safeCell(toReveal.x + 1, toReveal.y + 1) &&
             state[toReveal.x + 1][toReveal.y + 1] !== "r"
           ) {
-            arrToReveal.push({ x: toReveal.x + 1, y: toReveal.y + 1 });
+            insertCellInArray(
+              { x: toReveal.x + 1, y: toReveal.y + 1 },
+              arrToReveal
+            );
           }
         }
       }
@@ -124,7 +162,7 @@ function generateGame(height, width, bombs) {
       table[i][j] = cntBombs;
     }
   }
-  return { table, state, tryCell, flagCell, height, width };
+  return { table, state, height, width, stats, tryCell, flagCell };
 }
 
 // const game = generateGame(10, 10, 50);
